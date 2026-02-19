251e8905f1899228e5a484afe3f5c5ba51a315c7
Rahul95-eng <dubeyrahul76739@gmail.com>
2026-02-19 14:37:56 +0530

    Add files via upload

---
+++ v&c check.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{ 
+char a;
+printf("enter a character:");
+scanf("%c", &a);
+if (a=='A'|| a=='E'||a=='I'|| a=='O'||a=='U' ||a=='a'||a=='e'||a=='i'||a=='o'||a=='u' )
+{
+    printf("vowel");
+}
+else
+{ 
+    printf("constant");
+} 
+return 0;
+
+}
